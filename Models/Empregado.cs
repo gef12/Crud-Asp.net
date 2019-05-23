9a3a948d6d39c98e2bbc0015af8e475e7ccfc64a
gef12 <gerfersoncoelho@gmail.com>
2019-05-23 15:14:18 -0300

    ALterando views padroes e criando um controle paar os empregado

--- Models/Empregado.cs
+++ Models/Empregado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -14,16 +15,20 @@ namespace Crud_Asp.net.Models
         [Key]
         public int EmpregadoId { get; set; }
         [Column(TypeName = "nvarchar(250)")]
-        [Required]
+        [Required(ErrorMessage ="Este Campo Devera ser Prenchido")]
 
+        [DisplayName("Nome Completo")]
         public string NameCompleto { get; set; }
         [Column(TypeName = "varchar(20)")]
 
+        [DisplayName("Emp. Codigo")]
         public string EmpCode { get; set; }
         [Column(TypeName = "varchar(100)")]
 
         public string Posicao { get; set; }
         [Column(TypeName = "varchar(100)")]
+        [DisplayName("Localização do Trabalho")]
+
 
         public string LocalizacaoTrabalho { get; set; }
     }
