01adc7f2d7cdaa759cc11badbb60406024a4b234
gef12 <gerfersoncoelho@gmail.com>
2019-05-23 13:51:40 -0300

    Configurando a conexao ao banco e Injetando o serviço para tal

---
+++ Models/EmpregadoContexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_Asp.net.Models
+{
+    public class EmpregadoContexto : DbContext
+    {
+        //conexao com BD feita para os empregados
+        public EmpregadoContexto(DbContextOptions<EmpregadoContexto> options) : base(options)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Crud_Asp.net.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -32,6 +34,10 @@ namespace Crud_Asp.net
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //injetando dependecias para realizar a conexao do BD Sql
+            services.AddDbContext<EmpregadoContexto>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConexao")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
